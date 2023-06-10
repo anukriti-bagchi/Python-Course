be57e7a9bd7c5ec6da02b5214b72ec80cef82ca1
anukriti-bagchi <91126891+anukriti-bagchi@users.noreply.github.com>
2023-06-10 10:39:58 +0000

    resumed course

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world, welcome to 100 days of coding by  code with harry")
+print("hello world, welcome to 100 days of coding by code with harry")
+print("jolly")
+print("Jolly loves oma")
+"""
+Hey Jolly please dont remove this line 
+Author : Jolly 
+Course : 100 days of code by Code with Harry on Youtube
+"""
