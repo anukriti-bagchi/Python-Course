24b31bfc41129d93698bdd5c4194850f615052c1
anukriti-bagchi <91126891+anukriti-bagchi@users.noreply.github.com>
2023-06-11 12:52:14 +0000

    edited text

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ print("hello world, welcome to 100 days of coding by code with harry")
 print("jolly")
 """
 Hey Jolly please dont remove this line 
-Author : Jolly 
+Author : Anukriti
 Course : 100 days of code by Code with Harry on Youtube
 """
