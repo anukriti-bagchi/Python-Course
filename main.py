3aa4e8909e3401453712925a74e411578fc6f558
Anukriti <anukritibagchi98@gmail.com>
2023-06-12 22:10:42 +0530

    new changes

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-print("hello world, welcome to 100 days of coding by code with harry")
-print("jolly")
 """
-Hey Jolly please dont remove this line 
-Author : Anukriti
-Course : 100 days of code by Code with Harry on Youtube
+Hey Anu please dont remove this line.
+Author : Harry 
+Course : 100 days of code by Harry.
 """
+print('Hey I am\' a \"good girl\" \nand this viewer is also a good boy/girl')
+#print("Hello World")
+#This is a 'Single-line Comment'.
+#print("This is a print statement.")
+#print("Hello World") #printing hello world 
+
+print("hey", 6, 7, sep="~", end="009/n")
+print("Anu")
