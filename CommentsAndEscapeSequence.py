4240069ca83f799991c176ca21fa21bf7fc56f38
Anukriti <anukritibagchi98@gmail.com>
2023-06-12 22:26:33 +0530

    Merging Multiple files in to this branch

---
+++ CommentsAndEscapeSequence.py
@@ -0,0 +1,13 @@
+"""
+Hey Anu please dont remove this line.
+Author : Harry 
+Course : 100 days of code by Harry.
+"""
+print('Hey I am\' a \"good girl\" \nand this viewer is also a good boy/girl')
+#print("Hello World")
+#This is a 'Single-line Comment'.
+#print("This is a print statement.")
+#print("Hello World") #printing hello world 
+
+print("hey", 6, 7, sep="~", end="009/n")
+print("Anu")
